6c0198f32b9eae6c6c4402afa106a4c6a3694efc
amederos <amederos@amederos-mobl.amr.corp.intel.com>
2015-01-23 13:45:33 -0600

    update

--- mexicoder/mexicoder/settings.py
+++ mexicoder/mexicoder/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'uqz%!le)8yssk@n#6hm)zkydmj7_f1t-frf&g)q1hgdcxd$-vk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
 
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'screen'
+    'apps.screen',
+    'apps.REST',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,3 +83,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR), "static", "templates"),
+)
+
+
+if DEBUG:
+    MEDIA_URL = '/media/'
+    STATIC_ROOT = os.path.join(
+        os.path.dirname(BASE_DIR), "static", "static-only")
+    MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
+    STATICFILES_DIRS = (
+        os.path.join(os.path.dirname(BASE_DIR), "static", "static"),
+    )
